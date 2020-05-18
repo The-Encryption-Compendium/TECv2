d70f806d7f4df6504cb588b380c0c14b1a115768
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-05-18 01:35:02 -0600

    Add DEVELOPMENT.md, and deploy_tools/build_local.sh to make it easier to build the site locally.

---
+++ deploy_tools/build_local.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###
+### Build the site locally for testing purposes
+###
+
+CURRENT_DIR=$(dirname "$0")
+VENV_DIR="${CURRENT_DIR}/venv"
+
+# Download compendium entries locally
+wget https://raw.githubusercontent.com/The-Encryption-Compendium/the-encryption-compendium.github.io/master/data/data.bib \
+    -O "${CURRENT_DIR}/../data/data.bib"
+
+# Download dependencies
+python3 -m venv "${VENV_DIR}"
+source "${VENV_DIR}/bin/activate"
+python3 -m pip install -r "${CURRENT_DIR}/requirements.txt"
+python3 "${CURRENT_DIR}/get_dependencies.py"
+
+# Generate compendium entry files from data.bib
+python3 "${CURRENT_DIR}/generate_compendium.py" "${CURRENT_DIR}/../data/data.bib"
+
+# Generate HTML and CSS files with Hugo
+cd "${CURRENT_DIR}/.."
+hugo
